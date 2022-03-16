b60f53db234c1b77406e06146d74663a06ab21f8
SergeyEmelyanov <sergej-rabota@yandex.ru>
2022-03-16 19:42:39 +0300

    Второе изменение в Notepad++

--- Praktikum.java
+++ Praktikum.java
@@ -3,7 +3,7 @@ public class Praktikum {
 		System.out.println ("Первый коммит");
 		System.out.println ("Изменения в GitHub");
 		System.out.println ("Изменения в Notepad++");
-		System.out.println ("Изменения в GitHub2");
+		System.out.println ("Изменения в Notepad++2");
 		
 	}
 }
