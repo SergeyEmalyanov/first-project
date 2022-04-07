570ab3965eaebb3285edd806fe143accfd690a6d
SergeyEmelyanov <sergej-rabota@yandex.ru>
2022-04-07 15:50:38 +0300

    Первый2

--- Praktikum.java
+++ Praktikum.java
@@ -1,9 +1,10 @@
-public class Praktikum {
+﻿public class Praktikum {
 	public static void main (String [] args){
 		System.out.println ("Первый коммит");
 		System.out.println ("Изменения в GitHub");
 		System.out.println ("Изменения в Notepad++");
 		System.out.println ("Изменения в Notepad++2");
+                System.out.println ("Изменеия2");
 		
 	}
 }
